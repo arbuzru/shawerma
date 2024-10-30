e90c31c2b4a7adccfb7b5aebfb706ba4fe9f8820
арбузру <adadabaev784@gmail.com>
2024-10-30 18:54:53 +0500

    commit one

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,22 @@
     <link rel="stylesheet" href="{{ asset('css/venobox.min.css') }}">
 </head>
 <body>
-<div class="">
-    @yield('content')
-</div>
-<!-- Подключение скриптов -->
-<script src="{{ asset('js/jquery-3.6.3.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('js/aos.js') }}"></script>
-<script src="{{ asset('js/jquery.shuffle.min.js') }}"></script>
-<script src="{{ asset('js/slick.min.js') }}"></script>
-<script src="{{ asset('js/venobox.min.js') }}"></script>
-<script src="{{ asset('js/custom.js') }}"></script>
+
+    @include('partials.header') <!-- Включаем хедер -->
+
+    <div class="">
+         @yield('content')
+    </div>
+
+    @include('partials.footer') <!-- Включаем футер -->
+
+    <!-- Подключение скриптов -->
+    <script src="{{ asset('js/jquery-3.6.3.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/aos.js') }}"></script>
+    <script src="{{ asset('js/jquery.shuffle.min.js') }}"></script>
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/venobox.min.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
 </body>
 </html>
